b1bfc140627432faef3cf472fa0825ec6b354b01
Joshua Clark <clarkis117@live.com>
2016-10-26 01:32:02 -0400

    Initial Hello World Program based on Haywire's

--- Bindings/Unsafe/Structs.cs
+++ Bindings/Unsafe/Structs.cs
@@ -9,33 +9,83 @@ namespace HaywireNet.Bindings.Unsafe.Structs
 	[StructLayout(LayoutKind.Sequential)]
 	public unsafe struct HaywireString
 	{
-		byte* value;
-		uint length;
+		public byte* value;
+		public uint length;
+	}
+
+	/// <summary>
+	/// Set String methods just like haywire's
+	/// </summary>
+	public unsafe static class StringMethods
+	{
+		public static byte* GetASCII(string clrString)
+		{
+			var length = clrString.Length;
+
+			byte* bytes = (byte*)Marshal.AllocHGlobal(length);
+
+			fixed (char* chars = clrString)
+			{
+				System.Text.Encoding.ASCII.GetBytes(chars, length, bytes, length);
+			}
+
+			return bytes;
+		}
+
+		public static void SetStringASCII(out HaywireString hwString, string clrString)
+		{
+			var length = clrString.Length;
+
+			byte* bytes = (byte*)Marshal.AllocHGlobal(length);
+
+			fixed(char* chars = clrString)
+			{
+				System.Text.Encoding.ASCII.GetBytes(chars, length, bytes, length);
+			}
+
+			hwString.length = (uint)length;
+			hwString.value = bytes;
+		}
+
+		public static void SetStringUTF8(out HaywireString hwString, string clrString)
+		{
+			var length = clrString.Length;
+
+			byte* bytes = (byte*)Marshal.AllocHGlobal(length);
+
+			fixed (char* chars = clrString)
+			{
+				System.Text.Encoding.UTF8.GetBytes(chars, length, bytes, length);
+			}
+
+			hwString.length = (uint)length;
+			hwString.value = bytes;
+		}
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
 	public unsafe struct Configuration
 	{
-		byte* http_listen_address;
-		uint http_listen_port;
-		uint htread_count;
-		byte* parser;
-		bool tcp_nodelay;
-		uint listen_backlog;
-		uint max_request_size;
+		public byte* http_listen_address;
+		public uint http_listen_port;
+		public uint thread_count;
+		public byte* parser;
+		public bool tcp_nodelay;
+		public uint listen_backlog;
+		public uint max_request_size;
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
 	public unsafe struct HttpRequest
 	{
-		ushort http_major;
-		ushort http_minor;
-		byte method;
-		int keep_alive;
-		HaywireString* url;
-		void* headers;
-		HaywireString* body;
-		uint body_length;
-		public enum state { OK, SIZE_EXCEEDED, BAD_REQUEST, INTERNAL_ERROR };
+		public ushort http_major;
+		public ushort http_minor;
+		public byte method;
+		public int keep_alive;
+		public HaywireString* url;
+		public void* headers;
+		public HaywireString* body;
+		public uint body_length;
+		public enum state { OK = 0, SIZE_EXCEEDED, BAD_REQUEST, INTERNAL_ERROR };
 	}
 }
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,128 @@
+﻿using HaywireNet.Bindings.Unsafe;
+using HaywireNet.Bindings.Unsafe.Structs;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+	public unsafe class Program
+	{
+		const string RootRoute = "/";
+		const string PingRoute = "/ping";
+		const string DefaultAddress = "0.0.0.0";
+		const uint DefaultPort = 8000;
+
+		public static void Main(string[] args)
+		{
+			Configuration config;
+
+			config.http_listen_address = StringMethods.GetASCII(DefaultAddress);
+			config.http_listen_port = DefaultPort;
+			config.thread_count = 0;
+			config.parser = StringMethods.GetASCII("http_parser");
+			config.max_request_size = 1048576;
+			config.tcp_nodelay = true;
+			config.listen_backlog = 0;
+
+			Functions.hw_init_with_config(&config);
+
+			Functions.hw_http_add_route(StringMethods.GetASCII(RootRoute), GetRoot, null);
+
+			Functions.hw_http_add_route(StringMethods.GetASCII(PingRoute), GetPing, null);
+
+			Functions.hw_http_open();
+
+			return;
+		}
+
+		static void ResponseComplete(void* user_data)
+		{
+
+		}
+
+		static void GetRoot(HttpRequest* request, void* response, void* user_data)
+		{
+			HaywireString statusCode;
+			HaywireString contentTypeName;
+			HaywireString contentTypeValue;
+			HaywireString body;
+			HaywireString keepAliveName;
+			HaywireString keepAliveValue;
+			HaywireString routeMatchedName;
+			HaywireString routeMatchedValue;
+
+			StringMethods.SetStringASCII(out statusCode, "200 OK");
+
+			Functions.hw_set_response_status_code(response, &statusCode);
+
+			StringMethods.SetStringASCII(out contentTypeName, "Content-Type");
+
+			StringMethods.SetStringASCII(out contentTypeValue, "text/html");
+
+			Functions.hw_set_response_header(response, &contentTypeName, &contentTypeValue);
+
+			StringMethods.SetStringASCII(out body, "Hello World");
+			Functions.hw_set_body(response, &body);
+
+			if (request->keep_alive > 0)
+			{
+				StringMethods.SetStringASCII(out keepAliveName, "Connection");
+				StringMethods.SetStringASCII(out keepAliveValue, "Keep-Alive");
+				Functions.hw_set_response_header(response, &keepAliveName, &keepAliveValue);
+			}
+			else
+			{
+				Functions.hw_set_http_version(response, 1, 0);
+			}
+
+			Functions.hw_http_response_send(response, user_data, ResponseComplete);
+		}
+
+		static void GetPing(HttpRequest* request, void* response, void* user_data)
+		{
+			HaywireString statusCode;
+			HaywireString contentTypeName;
+			HaywireString contentTypeValue;
+			HaywireString body;
+			HaywireString keepAliveName;
+			HaywireString keepAliveValue;
+			HaywireString routeMatchedName;
+			HaywireString routeMatchedValue;
+
+			Functions.hw_print_request_headers(request);
+
+			//Functions.hw_print_body(request)
+
+			StringMethods.SetStringASCII(out statusCode, "200 OK");
+
+			Functions.hw_set_response_status_code(response, &statusCode);
+
+			StringMethods.SetStringASCII(out contentTypeName, "Content-Type");
+
+			StringMethods.SetStringASCII(out contentTypeValue, "text/html");
+
+			Functions.hw_set_response_header(response, &contentTypeName, &contentTypeValue);
+
+			body.value = request->body->value;
+			body.length = request->body->length;
+
+			Functions.hw_set_body(response, &body);
+
+			if (request->keep_alive > 0)
+			{
+				StringMethods.SetStringASCII(out keepAliveName, "Connection");
+				StringMethods.SetStringASCII(out keepAliveValue, "Keep-Alive");
+				Functions.hw_set_response_header(response, &keepAliveName, &keepAliveValue);
+			}
+			else
+			{
+				Functions.hw_set_http_version(response, 1, 0);
+			}
+
+			Functions.hw_http_response_send(response, user_data, ResponseComplete);
+		}
+	}
+}
